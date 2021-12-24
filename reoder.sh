8fc7b9799917d4a76d8fb35900265de3f2e1d58d
Daewon <0930now@naver.com>
2021-12-24 19:29:30 +0900

    0부터 시작

--- reoder.sh
+++ reoder.sh
@@ -4,12 +4,12 @@ echo "track no를 다시 순서로 맞춤"
 echo "입력으로 파일 이름, 순서로 파일이 필요"
 #cat $1
 
-no=1
+no=0
 diskno=1
-cat $1 | cut -d',' -f1 | while read filename
+cat $1 | cut -d',' -f2 | while read filename
 do
-	#echo "no $no, disk no $diskno" 
-	find ./ -type f -iname $filename -exec  mid3v2 -T "$no/$diskno" {} \;
+	echo "no $no, disk no $diskno" 
+	find ./ -type f -iname $filename -exec  mid3v2 -T "$no/$diskno" --TPOS "$diskno" {} \;
 	no=$(expr $no + 1 )
 	diskno=$(expr $no / 100 + 1 )
 done 
